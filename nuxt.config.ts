254a01b48599c3a885bc37405092643bd52e7144
Matteo Secco <matteo.secco@polimi.it>
2025-12-25 19:39:13 +0100

    Updated favicon(s)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,10 +12,15 @@ export default defineNuxtConfig({
         lang: 'en',
       },
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', href: '/favicon-96x96.png' },
+        {rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        {rel: 'shortcut icon', href: '/favicon.ico' },
+        {rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        {rel: 'manifest', href: '/site.webmanifest' },
       ],
       meta: [
         {name: 'author', content: 'Matteo Secco'},
+        {name: 'apple-mobile-web-app-title', content: 'Matteo Secco'},
       ]
     },
   },
